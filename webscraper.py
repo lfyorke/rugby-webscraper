9cf03ca0d167af8a4c2d8042863aaf0af83162df
Leo Yorke <Leo.Yorke@keyrus.com>
2017-11-10 11:09:50 +0000

    Split code out into functions to make it easier to understand and work with

--- webscraper.py
+++ webscraper.py
@@ -44,82 +44,98 @@ no_columns = {"Scoring": 7,
               "Defending": 5,
               "Discipline": 4}
 
-match_data = {"Scoring": 0,
+match_dict = {"Scoring": 0,
               "Attacking": 0,
               "Defending": 0,
               "Discipline": 0}
 
 
-def scrape_urls(url_list):
-    """ This function iterates over a list of urls and returns a dataframe of
-    results """
-    all_results = []
-    for url in url_list:
+def open_webpage(url):
+    """ This function opens a url with webdriver and returns it."""
+    try:
+        browser = webdriver.Chrome(executable_path="chromedriver.exe")
+        browser.get(url)
+        return browser
+    except:
+        return "Link not found"
+
+
+def get_match_data(browser):
+    """ This functions gets the match data (i.e. team names, score and competition) 
+        from an open webapge and returns them."""
+    match_data = {}
+    html = browser.page_source
+    soup = bs4.BeautifulSoup(html, "html.parser")
+    match_data["team_a"] = soup.find('div', class_='team team-a').find('span', class_='long-name').get_text()
+    match_data["team_a_score"] = soup.find('div', class_='team team-a').find('div', class_='score icon-font-after').get_text()
+    match_data["team_b"] = soup.find('div', class_='team team-b').find('span', class_='long-name').get_text()
+    match_data["team_b_score"] = soup.find('div', class_='team team-b').find('div', class_='score icon-font-before').get_text()
+    match_data["competition"] = soup.find('div', class_='game-details header').get_text()    
+    return match_data
+
+
+def get_player_data(browser):
+    """ This function returns the indivual players data from the open webpage and 
+        returns it."""
+    wait = WebDriverWait(browser, 5)
+    element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-container"]/div/div/div[1]/div[1]/div[1]/ul/li[1]')))
+    for key, value in xpaths.items():
+        skip = 0
         try:
-            browser = webdriver.Chrome(executable_path="chromedriver.exe")
-            browser.get(url)
-            wait = WebDriverWait(browser, 5)
-            element = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-container"]/div/div/div[1]/div[1]/div[1]/ul/li[1]')))
-            for key, value in xpaths.items():
-                skip = 0
-                try:
-                    element = browser.find_element_by_xpath(value)
-                    element.click()
-                    html = browser.page_source
-                    soup = bs4.BeautifulSoup(html, "html.parser")
-                    team_a = soup.find('div', class_='team team-a').find('span', class_='long-name').get_text()
-                    team_a_score = soup.find('div', class_='team team-a').find('div', class_='score icon-font-after').get_text()
-                    team_b = soup.find('div', class_='team team-b').find('span', class_='long-name').get_text()
-                    team_b_score = soup.find('div', class_='team team-b').find('div', class_='score icon-font-before').get_text()
-                    competition = soup.find('div', class_='game-details header').get_text()
-                    print(team_a, team_a_score,  team_b, team_b_score, competition)
-                    for data in soup.find_all('div', class_='col-b'):
-                        sub_match_list = []
-                        for tables in data.find_all("tbody"):
-                            for row in tables.find_all("tr"):
-                                player_row = []
-                                for element in row:
-                                    player_row.append(element.get_text())
-                                sub_match_list.append(player_row)
-                        df = pd.DataFrame(sub_match_list, columns=columns_labels[key])
-                        match_data[key] = df
-                except Exception as e:
-                    print("Link not found", e)
-                    skip = 1
-                    pass
-            browser.quit()
-            if skip == 0:
-                frames = []
-                for key, value in match_data.items():
-                    frames.append(value)
-                single_result = functools.reduce(lambda left,right: pd.merge(left,right,on='Player'), frames)
-                single_result["team"] = None
-                single_result["team"][0:23] = team_a
-                single_result["team"][23:] = team_b
-                single_result["score"] = None
-                single_result["score"][0:23] = team_a_score
-                single_result["score"][23:] = team_b_score
-                single_result["competition"] = competition
-                all_results.append(single_result)
-                print(single_result)
-            else:
-                pass
+            element = browser.find_element_by_xpath(value)
+            element.click()
+            html = browser.page_source
+            soup = bs4.BeautifulSoup(html, "html.parser")
+            for data in soup.find_all('div', class_='col-b'):
+                sub_match_list = []
+                for tables in data.find_all("tbody"):
+                    for row in tables.find_all("tr"):
+                        player_row = []
+                        for element in row:
+                            player_row.append(element.get_text())
+                        sub_match_list.append(player_row)
+                df = pd.DataFrame(sub_match_list, columns=columns_labels[key])
+                match_dict[key] = df
         except Exception as e:
-            print("There was a problem loading the page", e)
-            browser.quit()
-            break
-    return pd.concat(all_results)
+            print("Link not found", e)
+            skip = 1
+            pass
+    if skip == 0:
+        frames = []
+        for key, value in match_dict.items():
+            frames.append(value)
+        single_result = functools.reduce(lambda left,right: pd.merge(left,right,on='Player'), frames)
+        return single_result
+    else:
+        return None
+
+def combine_results(match_data, player_data):
+    """ This functions joins the player and match data and returns a single dataframe"""
+    player_data["team"] = None
+    player_data["team"][0:23] = match_data["team_a"]
+    player_data["team"][23:] = match_data["team_b"]
+    player_data["score"] = None
+    player_data["score"][0:23] = match_data["team_a_score"]
+    player_data["score"][23:] = match_data["team_b_score"]
+    player_data["competition"] = match_data["competition"]
+    return player_data
 
 
-def write_results(result_set):
+def write_results(all_data):
     """ This function writes a dataframe to a csv"""
-    return result_set.to_csv("match.csv", index=False)
+    return all_data.to_csv("match.csv", index=False)
+
 
 
 if __name__ == "__main__": 
-    #urls = generate_urls(5)
-    #print(urls)
-    urls = ['http://www.espn.co.uk/rugby/playerstats?gameId=291170&league=289234']
-    results = scrape_urls(urls)
-    write_results(results)
-    results.to_csv("match.csv", index=False)
+    urls = generate_urls(5)
+    all_results = []
+    for url in urls:
+        browser = open_webpage(url)
+        match_data = get_match_data(browser)
+        player_data = get_player_data(browser)
+        results = combine_results(match_data, player_data)
+        all_results.append(results)
+        browser.quit()
+    final = pd.concat(all_results)
+    write_results(final)
